870568138cd527cd7a4c03303928607d5712d713
Antonio Brandao <antoniobrandaodesign@gmail.com>
2022-10-21 10:57:49 +0100

    Fix testimonials

--- src/ui/components/botcamp/Testimonials.js
+++ src/ui/components/botcamp/Testimonials.js
@@ -4,7 +4,8 @@ import tw, { styled, css } from 'twin.macro'
 import { Section, Container } from '@hummingbot/hbui/elements/layout'
 import { P, H4, H6 } from '@hummingbot/hbui/elements/typography'
 import { CardB } from '@hummingbot/hbui/components/cards/CardB'
-import { Link } from 'react-router-dom' // import from 'gatsby' package in Gatsby projects
+
+import { Link } from 'gatsby'
 
 function Testimonials() {
   return (
@@ -12,70 +13,32 @@ function Testimonials() {
     <Container>
       <H4 isWhite isBold>Testimonials</H4>
       <Columns>
-        <Column>
-          <div tw='flex justify-between mb-md'>
-            <IconA tw='relative top-[4px]' />
-            <H6>Testimonial 1</H6>
-          </div>
-          <P tw='text-white'>Hummingbot is a local software client that never exposes your keys externally. The entire codebase is publicly auditable on Github</P>
-        </Column>
-        <Column>
-          <div tw='flex justify-between mb-md'>
-            <IconA tw='relative top-[4px]' />
-            <H6>Testimonial 2</H6>
-          </div>
-          <P tw='text-white'>We support most major centralized and decentralized exchanges, allowing you to hedge and arbitrage between them seamlessly</P>
-        </Column>
-        <Column>
-          <div tw='flex justify-between mb-md'>
-            <IconA tw='relative top-[4px]' />
-            <H6>Testimonial 3</H6>
-          </div>
-          <P tw='text-white'>With sophisticated strategies like Avellaneda, Hummingbot is the only trading bot designed for professional market makers and liquidity providers</P>
-        </Column>
-      </Columns>
-
-      {/* <div tw='w-full flex content-between'>
         <CardB
-          tw='mr-xs'
-          iconElement={<IconA/>}
+          tw='mb-4 lg:mb-0'
+          iconElement={<IconBook/>}
           headerText="Testimonial 1"
-          descriptionText="Inner site link. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus."
-          linkURL="/"
-          linkClass={Link}
-          external={false}
+          descriptionText="Hummingbot is a local software client that never exposes your keys externally. The entire codebase is publicly auditable on Github."
         />
         <CardB
-          tw='mr-xs'
-          iconElement={<IconB/>}
+          tw='mb-4 lg:mb-0'
+          iconElement={<IconBook/>}
           headerText="Testimonial 2"
-          descriptionText="With external link. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum."
-          linkURL="https://www.google.com"
-          external={true}
+          descriptionText="We support most major centralized and decentralized exchanges, allowing you to hedge and arbitrage between them seamlessly." // remove this if no link is necessary
         />
         <CardB
+          tw='mb-4 lg:mb-0'
+          iconElement={<IconBook/>}
           headerText="Testimonial 3"
-          descriptionText="With external link. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum."
-          linkURL="https://www.google.com"
-          linkClass={Link}
-          external={true}
+          descriptionText="With sophisticated strategies like Avellaneda, Hummingbot is the only trading bot designed for professional market makers and liquidity providers."
         />
-      </div>       */}
+      </Columns>
     </Container>
   </Section>
   )
 }
 
 const Columns = styled.div(() => [
-  tw`my-md grid grid-cols-1 gap-0 lg:(my-xl grid-cols-3 gap-8)`,
-])
-
-const Column = styled.div(() => [
-  tw`mb-sm p-sm lg:(p-md mb-0)`,
-  css`
-    background: #161C18;
-    border-radius: 10px;
-  `,
+  tw`my-md grid grid-cols-1 gap-0 lg:(my-xl grid-cols-3 gap-4)`,
 ])
 
 
@@ -99,7 +62,7 @@ function IconA() {
   );
 }
 
-function IconB() {
+function IconBook() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
