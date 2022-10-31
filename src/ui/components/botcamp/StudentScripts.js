6ecfb9932fada0f56723885ba44d015609251cb8
Antonio Brandao <antoniobrandaodesign@gmail.com>
2022-10-31 19:54:18 +0000

    Restyle StudentScripts

--- src/ui/components/botcamp/StudentScripts.js
+++ src/ui/components/botcamp/StudentScripts.js
@@ -5,34 +5,30 @@ import { Section, Container, Separator } from '@hummingbot/hbui/elements/layout'
 import { P, H1, H2, H4, H6 } from '@hummingbot/hbui/elements/typography'
 import StudentsJSON from "../../../../content/student-scripts.json"
 import { CardA } from '@hummingbot/hbui/components/cards/CardA'
+import { ExternalLinkButton } from '../../elements/buttons'
 
 function StudentScripts() {
   return (
-  <Section tw='pt-xxxl'>
+  <Section tw='py-28 md:py-40 lg:py-52 border-y-4 border-terminal' style={{background: 'linear-gradient(177deg, rgba(95, 255, 215, 0.15), black)'}}>
     <Container>
-        <H4 isBold tw='text-center'>
-          See what Botcamp students have built
-        </H4>
-        <P tw='text-secondary text-center mb-md'>
-          Scripts created by students in prior Botcamp cohorts
-        </P>
-        <ScriptsGrid>
-          {StudentsJSON.map((data, index) => {
-            return <CardA
-              key={index}
-              tw="mx-xs my-md" 
-              headerText={data.title}
-              descriptionText={data.description}
-              linkURL={data.link}
-              // {data.author} supervik
-              // {data.author_link} https://github.com/supervik
-              // {data.cohort} Sept 2022
-              external={true}
-            />
-          })}
-        </ScriptsGrid>
+      <H4 isBold tw='text-center leading-tight'>
+        See what Botcamp students have built
+      </H4>
+      <P tw='text-secondary text-center mb-lg md:mb-xxl'>
+        Scripts created by students in prior Botcamp cohorts
+      </P>
+      <ScriptsGrid>
+        {StudentsJSON.map((data, index) => {
+          return <div key={index} tw="mb-lg md:mb-0">
+            <P tw='font-bold text-terminal border-b-2 border-terminal w-full pb-xxs mb-xxs'>{data.title}</P>
+            <a href={data.link} target="_blank" rel="noreferrer">
+              <ExternalLinkButton label='View script' tw='w-full justify-between' />
+            </a>
+            <P tw='mt-xs'>{data.description}</P>
+          </div>
+        })}
+      </ScriptsGrid>
     </Container>
-    <Separator tw='pt-xxxl' />
   </Section>
   )
 }
@@ -40,5 +36,5 @@ function StudentScripts() {
 export default StudentScripts
 
 const ScriptsGrid = styled.div(() => [
-  tw`grid grid-cols-3 gap-0`,
+  tw`grid grid-cols-1 gap-0 md:(grid-cols-3 gap-4) lg:(md:gap-8)`,
 ])
