41141a0a0ee53bc34ebbff3597ded3b1c66cf2be
Michael Feng <mifeng@gmail.com>
2022-12-16 10:56:30 -0800

    clean up images and start hbot page

--- src/ui/components/hbot/Hero.js
+++ src/ui/components/hbot/Hero.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import tw, { styled } from 'twin.macro'
 import { Section, Container } from '@hummingbot/hbui/elements/layout'
-import { P, H1 } from '@hummingbot/hbui/elements/typography'
+import { P, H2 } from '@hummingbot/hbui/elements/typography'
 import { CustomRoundButton } from '../../elements/buttons'
 import { GradientSideLine } from '../../elements/layout'
 import { BackgroundNodes } from '../BackgroundNodes'
@@ -19,11 +19,17 @@ function Hero() {
       </span>
       <Section tw='h-[500px] md:h-[700px] lg:h-[800px]' style={{justifyContent: 'center'}}>
         <Container tw='z-10 pt-0 pb-7 md:pb-8 lg:pb-9 xl:pb-10'>
-          <CustomH1 isBold tw='lg:max-w-4xl pb-xs'>Crowd-sourced<br />Market Making</CustomH1>
-          <P tw='text-white max-w-[350px] mb-xs'>An alternative or complement to hiring a professional market maker</P>
-          <a href="https://miner.hummingbot.io" target='_blank' rel="noreferrer">
-            <CustomRoundButton isLarge>Get started</CustomRoundButton>
-          </a>
+          <CustomH1 isBold tw='lg:max-w-4xl pb-xs'>Decide how the Hummingbot codebase evolves</CustomH1>
+          <CustomContainer>
+            <LeftColumn>
+              <H2>Image</H2>
+            </LeftColumn>
+            <RightColumn>
+              <P>
+                The Hummingbot Governance Token (HBOT) lets holders decide which CEXs, DEXs, and strategies should be actively maintained and included in each Hummingbot release.
+              </P>
+            </RightColumn>
+          </CustomContainer>
         </Container>
       </Section>
       <BottomGradientLine />
@@ -33,13 +39,27 @@ function Hero() {
 
 export default Hero
 
-const CustomH1 = styled(H1)({
+const CustomH1 = styled(H2)({
   background: 'linear-gradient(270deg, #00C2CE 25.96%, #318DFF 100%)',
   WebkitTextFillColor: 'transparent',
   WebkitBackgroundClip: 'text',
   lineHeight: '100%',
 })
 
+const CustomContainer = styled(Container)(() => [
+  tw`flex flex-col my-md`,
+  tw`md:(flex-row my-xl)`,
+])
+
+const LeftColumn = styled.div(() => [
+  tw`w-full pb-xs`,
+  tw`md:(w-1/2 pr-xl)`,
+])
+
+const RightColumn = styled.div(() => [
+  tw`w-full`,
+  tw`md:(w-1/2)`,
+])
 
 const BottomGradientLine = styled.div({
   position: 'absolute',
